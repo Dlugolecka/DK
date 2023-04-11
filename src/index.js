ed87846d6f13f57fb224ac1a7afa16c2f29856a1
Dlugolecka <ewelina.dlugolecka1210@gmail.com>
2023-04-11 22:33:43 +0200

    Clear code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { style } from '../src/scss/index.scss';
-import createApp from '../src/js/app.js';
-import createHeader from '../src/js/header.js';
-import createFooter from '../src/js/footer.js';
-import createKeyboard from '../src/js/content.js'
+import { style } from "../src/scss/index.scss";
+import createApp from "../src/js/app.js";
+import createHeader from "../src/js/header.js";
+import createFooter from "../src/js/footer.js";
+import createKeyboard from "../src/js/content.js";
 
 let app = createApp();
 
--- src/js/footer.js
+++ src/js/footer.js
@@ -1,15 +1,14 @@
 function createFooter(app) {
-    let footer = document.createElement("footer");
-    let content = document.createElement('p')
+  let footer = document.createElement("footer");
+  let content = document.createElement("p");
 
-    content.textContent = "Drum Kit © 2023 ";
+  content.textContent = `Drum Kit © ${new Date().getFullYear()}`;
 
-    footer.className = "footer--container ";
-    content.className = "footer--content";
-  
-    footer.appendChild(content);
-    app.appendChild(footer);
-  }
-  
-  module.exports = createFooter;
-  
+  footer.className = "footer--container ";
+  content.className = "footer--content";
+
+  footer.appendChild(content);
+  app.appendChild(footer);
+}
+
+module.exports = createFooter;
